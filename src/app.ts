3c9dcc836da864806490bf0ce199a67189ecc440
Dev.Prabhat <76727816+Prabhat2373@users.noreply.github.com>
2023-10-17 22:32:43 +0530

    changes

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,6 @@ app.use(
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 // ROUTES
 app.use("/api/v1", userRouter);
 app.use("/api/v1", jobRouter);
